0f7bc816b2c6e380237b78e1caeb495a9e1ee26f
cbalestieri <camille.balestieri@gmail.com>
2020-02-09 21:33:23 -0300

    "Code Review das telas de Splash e login"

--- app/src/main/java/br/com/elasnojogo2/SplashActivity.java
+++ app/src/main/java/br/com/elasnojogo2/SplashActivity.java
@@ -26,7 +26,7 @@ public class SplashActivity extends AppCompatActivity {
     private void jump () {
         timer.cancel();
 
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
         finish();
     }
 }
