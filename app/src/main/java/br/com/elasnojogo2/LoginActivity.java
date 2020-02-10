ed77d857c3a858554a7b1f6416d8d2266a4b5f43
cbalestieri <camille.balestieri@gmail.com>
2020-02-09 21:07:50 -0300

    "Code Review das telas de Splash e login"

--- app/src/main/java/br/com/elasnojogo2/LoginActivity.java
+++ app/src/main/java/br/com/elasnojogo2/LoginActivity.java
@@ -1,19 +1,15 @@
 package br.com.elasnojogo2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class MainActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
+
+
