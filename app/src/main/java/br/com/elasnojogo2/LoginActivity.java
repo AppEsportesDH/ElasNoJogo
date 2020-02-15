3444583dfe29aeed8b1ab20b5a15fa2a082928f0
cbalestieri <camille.balestieri@gmail.com>
2020-02-15 06:26:56 -0300

    Correção Login - Karol

--- app/src/main/java/br/com/elasnojogo2/LoginActivity.java
+++ app/src/main/java/br/com/elasnojogo2/LoginActivity.java
@@ -1,5 +1,4 @@
 package br.com.elasnojogo2;
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,30 +6,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import com.google.android.material.textfield.TextInputLayout;
-
-
 public class LoginActivity extends AppCompatActivity {
     private TextInputLayout email;
     private TextInputLayout senha;
     Button botao_cadastro;
     Button botao_entrar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         botao_entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //A Activity de Menu ainda será criada
+                String email1 = email.getEditText().getText().toString();
+                String senha1 = senha.getEditText().getText().toString();
+                validarEmailSenha(email1, senha1);
             }
         });
     }
-
-    private boolean validarEmail() {
-        String emailInput = email.getEditText().getText().toString();
-        if (emailInput.isEmpty()) {
+    private boolean validarEmailSenha(String emailInput, String senhaInput) {
+        if (emailInput.isEmpty() && senhaInput.isEmpty()) {
             email.setError("Preencha os campos!");
             return false;
         } else {
@@ -38,29 +34,19 @@ public class LoginActivity extends AppCompatActivity {
             return true;
         }
     }
-
-    private boolean validarSenha() {
-        String senhaInput = senha.getEditText().getText().toString().trim();
-        if (senhaInput.isEmpty()){
-            senha.setError("Preencha os campos");
-            return false;
-        } else {
-            senha.setError(null);
-            return true;
-        }
-
-        private boolean validarCampos(){
-            if (!validarEmail()|| !validarSenha()){
-                return false;
-            }
-            String input = "Email: " + email.getEditText().getText().toString();
-            input += "\n";
-            input+= "Senha: " + senha.getEditText().getText().toString();
-            Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public void initViews( ){
+    //    }
+//
+//    private boolean validarCampos(View view) {
+//        String input = "Email: " + email.getEditText().getText().toString();
+//        input += "\n";
+//        input += "Senha: " + senha.getEditText().getText().toString();
+//        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+//        if (!validarEmail() || !validarSenha()) {
+//
+//        }
+//        return false;
+//    }
+    public void initViews() {
         email = findViewById(R.id.campo_email);
         senha = findViewById(R.id.campo_senha);
         botao_cadastro = findViewById(R.id.botao_cadastro);
